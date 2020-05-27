56ec5598fc8dc31feab123895fb804595c6d78ba
Daryl Browne <daryl.browne@googlemail.com>
2020-05-27 17:56:03 +0100

    feat(admin):  update current lystIitems

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import { createUserProfile, updateAccountDisplayName, ugradeAnnoymousUser } from
 import { getImagesFromUrl } from "./puppeteer";
 import { generateThumbnails, removeImageAndThumbsOnLystItemDelete, removeImageThumbsOnRemove } from "./images";
 import { deleteAllLystItems, deleteAllAnonymousUsers, removeCategoriesFromLystItem, deleteAllCategories } from "./triggers";
+import { moveAllNestedLystItemsToRoot, fixCategoryData } from "./admin";
 
 // if (process.env.NODE_ENV !== "production") {
 //   const credential = require("./gift-wishlyst-firebase-adminsdk-pt1at-0509e450c6.json");
@@ -28,4 +29,6 @@ module.exports = {
   deleteAllCategories,
   increaseClaimedLystItemsCount,
   decreaseClaimedLystItemsCount,
+  moveAllNestedLystItemsToRoot,
+  fixCategoryData,
 };
