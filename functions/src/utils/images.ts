6edc5a628ab85b343cfcc080ae6a66c716179b35
Daryl Browne <daryl.browne@googlemail.com>
2020-05-30 22:32:36 +0100

    feat(functions/utils): remove thumbnail utility functions

--- functions/src/utils/images.ts
+++ functions/src/utils/images.ts
@@ -33,52 +33,6 @@ export const resizeOriginalImage = async (filePath: string, contentType: string,
   return true;
 };
 
-export const resizeIntoThumbs = (filePath: string, downloaedImgPath: string, metadata: any, bucketName?: string) => {
-  const fileName = path.basename(filePath);
-  const genTempName = getTempFilename(filePath);
-  const bucket = admin.storage().bucket(bucketName);
-
-  const uploadPromises = sizes.map(async size => {
-    const thumbName = genSizeFileName(size, fileName);
-    const thumbPath = genTempName(thumbName);
-    const saveLocation = path.join(path.dirname(filePath), thumbName);
-    // Resize source image
-    await sharp(downloaedImgPath)
-      .resize(size, size, { withoutEnlargement: true })
-      .toFile(thumbPath);
-
-    // Upload to GCS
-    return bucket.upload(thumbPath, { destination: saveLocation, metadata: metadata });
-  });
-
-  return Promise.all(uploadPromises);
-};
-
-export const createThumbs = async (fileBucket: string, filePath: string, contentType: string, fileMetadata: { [key: string]: any }) => {
-  const fileName = path.basename(filePath);
-
-  if (!contentType.startsWith("image/")) return;
-  if (fileName.startsWith("thumb@")) return;
-  if (fileMetadata.resized) return;
-
-  // Download file from bucket.
-  const bucket = admin.storage().bucket(fileBucket);
-  const workingDir = path.join(os.tmpdir(), fileName.substring(0, fileName.lastIndexOf(".")));
-  const tempFilePath = path.join(workingDir, fileName);
-  await fs.ensureDir(workingDir);
-  await bucket.file(filePath).download({ destination: tempFilePath });
-  console.log("Image downloaded locally to", tempFilePath);
-
-  const metadata = { contentType: contentType, metadata: { resized: true } };
-
-  await Promise.all([resizeIntoThumbs(filePath, tempFilePath, metadata), resizeOriginalImage(filePath, contentType)]);
-
-  // Once the thumbnail has been uploaded delete the local file to free up disk space.
-  // fs.removeSync(workingDir);
-
-  return filePath;
-};
-
 export const removeThumbs = (filePath: string) => {
   const bucket = admin.storage().bucket();
   const fileName = path.basename(filePath);
