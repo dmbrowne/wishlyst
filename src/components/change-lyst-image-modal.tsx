32ba1e8e3a7abd49da01577b6e823d46966fb00e
Daryl Browne <daryl.browne@googlemail.com>
2020-05-31 02:10:53 +0100

    refactor(lystImageModal): use download urls instead of storage refs

--- src/components/change-lyst-image-modal.tsx
+++ src/components/change-lyst-image-modal.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useState, useContext } from "react";
 import Modal from "./modal";
 import SObjectFitImage from "../styled-components/object-fit-image";
-import FirebaseImage from "./firebase-image";
 import { Box, Tabs, Tab, ResponsiveContext } from "grommet";
 import ImageUpload from "./image-upload";
 import ImageUploadPlaceholder from "./image-upload-placeholder";
@@ -18,19 +17,19 @@ type IOnSubmitProps =
     };
 interface Props {
   onClose: () => void;
-  activeRef?: string;
+  activeImgUrl?: string;
   customUploadRef: string;
   onSubmit: (props: IOnSubmitProps) => any;
 }
 
-const ChangeLystImageModal: FC<Props> = ({ onClose, activeRef, onSubmit, customUploadRef }) => {
+const ChangeLystImageModal: FC<Props> = ({ onClose, activeImgUrl, onSubmit, customUploadRef }) => {
   const [selected, setSelected] = useState("");
   const isMobile = useContext(ResponsiveContext) === "small";
 
-  const stockImageRefs = [
-    "defaults/lyst_images/stock-gifts.svg",
-    "defaults/lyst_images/jfbv_qmpo_171226.jpg",
-    "defaults/lyst_images/cakes.svg",
+  const stockImageUrls = [
+    "https://firebasestorage.googleapis.com/v0/b/gift-wishlyst.appspot.com/o/defaults%2Flyst_images%2Fstock-gifts.svg?alt=media&token=d3b2155e-c78e-4f5d-a8ac-97c45d8b76f0",
+    "https://firebasestorage.googleapis.com/v0/b/gift-wishlyst.appspot.com/o/defaults%2Flyst_images%2Fjfbv_qmpo_171226.jpg?alt=media&token=73a76851-43c5-4234-b22f-548670a54f74",
+    "https://firebasestorage.googleapis.com/v0/b/gift-wishlyst.appspot.com/o/defaults%2Flyst_images%2Fcakes.svg?alt=media&token=22de6a52-9fd1-4a3d-8fd6-7076cb56f2a7",
   ];
 
   return (
@@ -38,24 +37,24 @@ const ChangeLystImageModal: FC<Props> = ({ onClose, activeRef, onSubmit, customU
       title="Change lyst image"
       onClose={onClose}
       primaryActions={[
-        { label: "Change", onClick: () => onSubmit(selected) },
+        { label: "Change", onClick: () => onSubmit({ isCustomImage: false, downloadUrl: selected }) },
         { label: "Cancel", onClick: onClose },
       ]}
     >
       <Tabs>
         <Tab title="Library">
           <Box gap="medium" margin={{ top: "large" }} direction="row">
-            {stockImageRefs.map(imageRef => {
-              const isActive = activeRef === imageRef;
-              const isSelected = imageRef === selected;
+            {stockImageUrls.map(imageUrl => {
+              const isActive = activeImgUrl === imageUrl;
+              const isSelected = imageUrl === selected;
               return (
                 <Box
-                  key={imageRef}
-                  onClick={() => setSelected(imageRef)}
+                  key={imageUrl}
+                  onClick={() => setSelected(imageUrl)}
                   style={{ width: isMobile ? "33%" : "25%", height: isMobile ? 100 : 150, borderRadius: 12, overflow: "hidden" }}
                   border={isActive ? { size: "5px", color: "brand" } : isSelected ? { color: "brand" } : true}
                 >
-                  <FirebaseImage imageRef={imageRef} children={url => <SObjectFitImage src={url} />} />
+                  <SObjectFitImage src={imageUrl} />
                 </Box>
               );
             })}
@@ -66,7 +65,10 @@ const ChangeLystImageModal: FC<Props> = ({ onClose, activeRef, onSubmit, customU
             <ImageUpload
               name="lyst-header"
               uploadRefPath={customUploadRef}
-              onUploadSuccess={uploadSnap => onSubmit(uploadSnap.ref.fullPath)}
+              onUploadSuccess={async uploadSnap => {
+                const downloadUrl = await uploadSnap.ref.getDownloadURL();
+                onSubmit({ isCustomImage: true, storageRef: uploadSnap.ref.fullPath, downloadUrl });
+              }}
             >
               {({ name, onUpload }) => <ImageUploadPlaceholder onInputFileChange={onUpload} name={name} />}
             </ImageUpload>
